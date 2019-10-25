4f816ebab2cf840a30a38fd16fd6ae7fb780c1d5
Rishabh <princerishabh.sharma400@gmail.com>
2019-10-25 18:58:04 +0530

    Added Forms

---
+++ medical/forms.py
@@ -0,0 +1,34 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
+from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from medical.models import User, MedicalHistory
+
+
+class UserRegistrationForm(FlaskForm):
+    name = StringField('Name',
+                       validators=[DataRequired(), Length(min=2, max=20)])
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    confirm_password = PasswordField('Confirm Password',
+                                     validators=[DataRequired(), EqualTo('password')])
+    age = StringField('Age', validators=[DataRequired(), Length(min=1, max=3)])
+    address = StringField('Address', validators=[DataRequired()])
+    gender = StringField("M or F", validators=[DataRequired()])
+    submit = SubmitField('Sign Up')
+
+    def validate_email(self, email):
+        patient = Patient.query.filter_by(email=email.data).first()
+        if patient:
+            raise ValidationError(
+                'That email is taken. Please choose a different one.')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email',
+                        validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember = BooleanField('Remember Me')
+    submit = SubmitField('Login')
